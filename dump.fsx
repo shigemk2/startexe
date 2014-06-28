583a6bb881cd7d68a7b64185aa56131bb326e43d
shigemk2 <i.am.shige@gmail.com>
2014-06-28 14:44:04 +0900

    This is 1st commit

---
+++ dump.fsx
@@ -0,0 +1,17 @@
+let aout = System.IO.File.ReadAllBytes "./bitmap.exe"
+printfn "File Size = 0x%x" aout.Length
+for i in 0 .. 16 .. aout.Length - 1 do
+    printf "%08x " i
+    for j in 0 .. 15 do
+        if i + j < aout.Length then
+            printf " %02x " aout.[i + j]
+        else
+            printf "    "
+    for j in 0 .. 15 do
+        if i + j < aout.Length then
+            let n = int aout.[i + j]
+            if 0x20 <= n && n <= 0x7e then
+                printf "%c" (char n)
+            else
+            printf "."
+    printfn ""
