78fde504d4641d0dbc600e9504ca463fb7fac2c0
shigemk2 <i.am.shige@gmail.com>
2014-06-29 14:48:50 +0900

    Execute with parameter

--- brainfuck.c
+++ brainfuck.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    FILE *f = fopen("helloworld.b", "r");
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("source nothing\n");
+        exit(1);
+    }
+    FILE *f = fopen(argv[1], "r");
     char bf[65536];
     int c;
     fgetc(f);
