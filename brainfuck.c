519ea586b4685aee62e87f8fdee37f60e86a4e9c
shigemk2 <i.am.shige@gmail.com>
2014-06-29 14:09:15 +0900

    apply fread

--- brainfuck.c
+++ brainfuck.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 
 int main(void) {
-    FILE *f = fopen("test.b", "rb");
+    FILE *f = fopen("test.b", "r");
+    char bf[65536];
     int c;
     fgetc(f);
     int reg[8];
     reg[0] = 0;
     int curreg = 0;
+    int i = 0;
+    int start = 0;
 
-    while( (c = fgetc( f )) != EOF ){
-        /* printf( "%c", c ); */
-        if (c == '+') {
+    int len = fread(bf, 1, sizeof(bf), f);
+    while( i <= sizeof(bf) ){
+        if (bf[i] == '+') {
             reg[curreg] += 1;
         }
-        if (c == '-') {
+        if (bf[i] == '-') {
             reg[curreg] -= 1;
         }
-        if (c == '>') {
+        if (bf[i] == '>') {
             curreg += 1;
         }
-        if (c == '<') {
+        if (bf[i] == '<') {
             curreg -= 1;
         }
-        if (c == '.') {
-            printf( "%d", reg[curreg]);
-            printf( "%c", reg[curreg]);
+        if (bf[i] == '[') {
+            start = i;
         }
+        if (bf[i] == ']') {
+            i = start;
+        }
+        if (bf[i] == '.') {
+            printf( "%c", reg[curreg]);
+            return 0;
+        }
+        i += 1;
     }
-    printf( "%d\n", reg[0]);
-    printf( "%c\n", reg[0]);
-
     fclose(f);
     return 0;
 }
