82b5355ae988751d3060b9a1de313e0c89cee2c0
shigemk2 <i.am.shige@gmail.com>
2014-06-29 13:44:31 +0900

    Apply +-><

--- brainfuck.c
+++ brainfuck.c
@@ -4,10 +4,31 @@ int main(void) {
     FILE *f = fopen("test.b", "rb");
     int c;
     fgetc(f);
+    int reg[8];
+    reg[0] = 0;
+    int curreg = 0;
 
     while( (c = fgetc( f )) != EOF ){
-        printf( "%c", c );
+        /* printf( "%c", c ); */
+        if (c == '+') {
+            reg[curreg] += 1;
+        }
+        if (c == '-') {
+            reg[curreg] -= 1;
+        }
+        if (c == '[') {
+            curreg += 1;
+        }
+        if (c == ']') {
+            curreg -= 1;
+        }
+        if (c == '.') {
+            printf( "%d", reg[curreg]);
+            printf( "%c", reg[curreg]);
+        }
     }
+    printf( "%d\n", reg[0]);
+    printf( "%c\n", reg[0]);
 
     fclose(f);
     return 0;
